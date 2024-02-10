706c36139b1a02e182e9b7e67feaf2001766023d
Pratiksha-Auti <pratikshaauti004@gmail.com>
2024-02-11 02:54:01 +0530

    ASCII_Code

---
+++ Class_Codes/3_ASCII_code/program20.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main(){
+	int x;
+	printf("Enter value\n");
+	scanf("%d",&x);
+	printf("value of x=%d\n",x);
+}
---
+++ Class_Codes/3_ASCII_code/program21.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main(){
+	int x;
+	printf("Enter value\n");
+	scanf("%c",&x);
+	printf("value of x=%\n",x);
+}
